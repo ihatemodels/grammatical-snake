e542f1c7e9a1bde0a25db7c1ec317335f1f28582
Gergin Darakov <47652045+ihatemodels@users.noreply.github.com>
2019-09-06 02:29:32 +0000

    Update spellcheck.py
    Version=0.1

--- spellcheck.py
+++ spellcheck.py
@@ -1,43 +1,67 @@
-import argparse
-import re
-## Local imports 
-
+import argparse,textwrap,re
+from argparse import RawTextHelpFormatter
 from dicts.bg import Bulgarian
 from dicts.en import English
 
-# Check the input language
 
-def is_cyrillic(word):
-    return bool(re.search("[а-яА-Я]", word))
+__author__ = 'Gergin Darakov'
+__version__ = 0.1
+__credits__ = 'Credits for all scrapped dictionaries in README'
+__status__ = 'Development'
+__license__ = 'GPL-3.0'
+
 
 def main():
-    
-    parser = argparse.ArgumentParser(
-        description='Termianl spell-check, word-meaning, synonyms, translate and many more')
 
-    parser.add_argument(
-        '--details',
-        '-d',
-        dest='details',
-        help='Pass the argument to display examples and synonyms.',
-        action='store_true'    
-    )
+    ''' The execution point of the script the --input argument
+       isn't set as required via argparse, cuz it will conflict
+       with other new args when the script becomes big  '''
+
+    def is_cyrillic(word):
+        '''Verify the input language '''
+        return bool(re.search("[а-яА-Я]", word))
+
+    parser = argparse.ArgumentParser(
+        description='Simple terminal dictionary with advanced features',
+        epilog=textwrap.dedent('''
+        {}
+        Author: {}
+        Version: {}'''.format(__credits__,__author__,__version__)),
+        formatter_class=RawTextHelpFormatter)
 
     parser.add_argument(
         '--input',
         '-i',
         dest='word',
-        help='Pass a word in bulgarian or in english. In english the word must be singular'
-    )
+        type=str,
+        help=textwrap.dedent('''\
+        [*] required
+        Word in Bulgarian or in English(singular). The script will
+        detect the input language. If the word is correct and exists
+        word forms will be returned. Else suggestions will be displayed.
+
+        '''))
+
+    parser.add_argument(
+        '--details',
+        '-d',
+        dest='details',
+        action='store_true',
+        help=textwrap.dedent('''\
+        Pass to display definition, examples,
+        synonyms, translate (BG to EN only atm)
+        when available.
+
+        '''))
 
     args=parser.parse_args()
     word = args.word
     details = args.details
-    
+
     if not word:
         print('Please specify a word')
         exit()
-        
+
     if is_cyrillic(word):
         new_word = Bulgarian(word,details)
         new_word.display()
@@ -45,8 +69,5 @@ def main():
         new_word = English(word,details)
         new_word.display()
 
-
-
 if __name__ == "__main__":
     main()
-
