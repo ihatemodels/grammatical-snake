e67634d8dd265dfdfb2e66a0e1247775aa09d6f1
Gergin Darakov <47652045+ihatemodels@users.noreply.github.com>
2019-09-06 02:30:18 +0000

    Update bg.py

--- dicts/bg.py
+++ dicts/bg.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 class Bulgarian:
+    ''' A class for validating and extending
+    bulgarian words. The main goal of the project
+    is to present the scrapped information as clear and
+    readable as possible. As the project grows
+    the idea behind is to add more complex features like
+    localdatabase, text-analyse, grammar-mode in which
+    the user can search for a specific grammar rule by
+    given query.'''
+
     def __init__(self, word, details):
 
         self.word = word
@@ -18,9 +26,10 @@ class Bulgarian:
         if self.is_details:
             self.set_meaning_syns()
 
-    """ spellcheck the given word set the is_correct and fill the self.forms"""
-
     def set_spellcheck(self):
+
+        ''' spellcheck the word set the is_correct and
+        fill the self.forms '''
 
         spellcheck = requests.get(
             "https://slovored.com/search/pravopisen-rechnik/" + self.word
@@ -36,10 +45,11 @@ class Bulgarian:
             self.is_correct = True
             self.forms = spellcheck.find("pre").get_text()
 
-    """ setting the meaning, synonyms and the tranlasted form of the given word
-       if there is no meaning or syns the variables will be empty """
-
     def set_meaning_syns(self):
+
+        ''' setting the meaning, synonyms and
+            the tranlasted form if there is no
+            meaning or syns the variables will be empty '''
 
         data = requests.get("http://rechnik.info/" + self.word)
         mistake = BeautifulSoup(data.content, "html.parser").find(class_="word_no_desc")
@@ -49,7 +59,7 @@ class Bulgarian:
         data = BeautifulSoup(data.content, "html.parser").find_all(class_="defbox")
 
         if not mistake:
-            
+
             for header,element in zip(headers,data):
                 if "Тълковен речник" in header.get_text():
                     self.meaning = element.get_text()
@@ -57,23 +67,21 @@ class Bulgarian:
                     self.synonyms = element.get_text()
                 if "Българо-Английски речник" in header.get_text():
                     self.translated = element.get_text()
-    
         else:
             pass
 
-
-    """ Check which arguments are passed and
-       display the variables in human readable format  """
-
     def display(self):
 
-        """ initializing color variables """
+        ''' Check which arguments are passed and
+            display the variables in human readable
+            format  '''
+
+        ''' initializing color variables '''
 
         reset = "\033[0m"
         yellow = "\033[93m"
         red = "\033[91m"
         cyan = "\033[36m"
-
 
         if self.is_correct:
             print(yellow)
@@ -81,22 +89,25 @@ class Bulgarian:
             print(self.forms.rstrip())
 
             if self.is_details:
-                print(yellow)
-                print("\n[-] Синоними:\n",cyan)
-                print(self.synonyms)
-                print(yellow)
-                print("[*] Tълковен речник:\n",reset)
-                print(self.meaning)
-                print(yellow)
-                print("[+] Превод:\n",reset)
-                print(self.translated)
+                if self.synonyms:
+                    print(yellow)
+                    print("\n[-] Синоними:\n",cyan)
+                    print(self.synonyms)
+                if self.meaning:
+                    print(yellow)
+                    print("[*] Tълковен речник:\n",reset)
+                    print(self.meaning)
+                if self.translated:
+                    print(yellow)
+                    print("[+] Превод:\n",reset)
+                    print(self.translated)
         else:
             print(red,"[!!]")
             print(yellow,self.error,reset)
 
-    ##########################################################
-
-    """ methods to return back the scraped values"""
+    ''' Methods to return back the scraped values.
+        This is for later when the database
+        idea becomes reality '''
 
     def get_synonyms(self):
 
@@ -113,3 +124,4 @@ class Bulgarian:
     def get_translate(self):
 
         return self.translated
+
